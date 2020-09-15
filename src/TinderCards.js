7cd7a9e297111bcc8d1bec877b9e02c8ea1ec05a
Jia <jiahui816@gmail.com>
2020-09-15 21:34:41 +1000

    Update TinderCards.js

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,7 +1,42 @@
-import React from "react";
+import { SwipeableDrawer } from "@material-ui/core";
+import React, { useState } from "react";
 import "./TinderCards.css";
 function TinderCards() {
-  return <div className="tinderCards"></div>;
+  const [people, setPeople] = useState([
+    {
+      name: "Elon Musk",
+      url:
+        "https://static.theceomagazine.net/wp-content/uploads/2018/10/15093202/elon-musk-1100x733.jpg",
+    },
+  ]);
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing" + nameToDelete);
+  };
+  const outOfFrame = (name) => {
+    console.log(name + " Left the screen!");
+  };
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCards
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCards>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TinderCards;
